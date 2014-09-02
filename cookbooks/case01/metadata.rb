7bb2dfa76ac4556f119c900a15d78a0104ff205e
k2works <kakimomokuri@gmail.com>
2014-09-02 14:12:15 +0900

    セットアップ

---
+++ cookbooks/case01/metadata.rb
@@ -0,0 +1,8 @@
+name             ''
+maintainer       ''
+maintainer_email ''
+license          ''
+description      'Installs/Configures '
+long_description 'Installs/Configures '
+version          '0.1.0'
+
